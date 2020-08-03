de84f4c8a1881cdece22b1d4ef830338b452148e
unknown <survase.ajay06@gmail.com>
2020-08-03 14:25:13 +0530

    third commit

--- urlshortner/urls.py
+++ urlshortner/urls.py
@@ -14,11 +14,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from basic import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('<str:token>', views.Home, name='Home'),
     path('', views.Make, name="Make new"),
+    path('', include('infra.urls')),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
